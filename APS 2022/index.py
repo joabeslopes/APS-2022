bcbe960475604aeddd193676f613db40d8b62336
Joabe Lopes <104812737+joabeslopes@users.noreply.github.com>
2022-11-15 22:49:54 -0300

    Update index.py

--- APS 2022/index.py
+++ APS 2022/index.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-tabela = pd.read_csv('medicamentos.csv', delimiter =';', encoding='latin-1', decimal=',')
+tabela = pd.read_csv('medicamentos.csv', delimiter =';', encoding='latin-1')
 
 print(tabela.head(9))
